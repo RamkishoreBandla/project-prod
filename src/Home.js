784bd751ce3079acafcf47cd4e0db4feb7172d3c
RamkishoreBandla <bandlaramkishore090@gmail.com>
2024-04-14 00:08:30 -0400

    validators

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,23 @@ const Home=()=>{
 
     return(
         <>
-        wELCOME TO EDGE PAIR PATH COVERAGE TOOL 
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col">
+                <h1> WELCOME TO EDGE PAIR PATH COVERAGE TOOL</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p>
+                    Here you will find instructions
+
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        
         </>
     )
 }
